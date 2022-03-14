dceeb39bcc74412d6603d0c78ecfdbec2a46013c
jeremy aubrey <jeremyaubrey@gmail.com>
2022-03-13 19:45:01 -0500

    calculates prime using streams

--- src/PrimeCallable.java
+++ src/PrimeCallable.java
@@ -1,12 +1,41 @@
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class PrimeCallable implements Callable<IntStream>{
+public class PrimeCallable implements Callable<List<Integer>> {
+
+	int start;
+	int end;
+
+	public PrimeCallable(int start, int end) {
+
+		this.start = start;
+		this.end = end;
+	}
+
+	private boolean isPrime(int num) {
+		boolean isPrime = false;
+		
+		if (num <= 2) {
+			isPrime = num == 2;
+		} else {
+			return (num % 2) != 0 && IntStream.rangeClosed(3, (int) Math.sqrt(num))
+					.allMatch(i -> num % i != 0);
+		}
+		
+		return isPrime;
+	}
 
 	@Override
-	public IntStream call() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Integer> call() throws Exception {
+		
+		List<Integer> primes = IntStream.rangeClosed(start, end)
+		.filter(num -> isPrime(num))
+		.boxed()
+		.collect(Collectors.toList());
+		
+		return primes;
 	}
 
 }
